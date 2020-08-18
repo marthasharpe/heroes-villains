eaaaf69185b469a990336f52732eb71cd3eb8a4c
Martha Sharpe <freedom@Nats-MacBook-Pro.local>
2020-08-17 20:40:10 -0400

    create state and useEffect blocks for fetching data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBar from './components/SearchBar';
 import CharacterCard from './components/CharacterCard';
+import { Container } from 'react-bootstrap';
+
 
 function App() {
+
+  const [ characterInfo, setCharacterInfo ] = useState();
+  const [ characterName, setCharacterName ] = useState('supergirl')
+
+  useEffect(() => {
+    fetch(`https://superheroapi.com/api/10104513882181015/search/${characterName}`)
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }, [])
+
   return (
-    <div className="App">
+    <Container>
       <h1>Heroes & Villains</h1>
       <SearchBar />
       <CharacterCard />
-    </div>
+    </Container>
   );
 }
 
