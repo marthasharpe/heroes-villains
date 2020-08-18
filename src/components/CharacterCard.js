0c32f5fb01ee0a7239bdd29a0393750e446cf9d3
Martha Sharpe <freedom@Nats-MacBook-Pro.local>
2020-08-17 20:22:07 -0400

    create basic file structure

---
+++ src/components/CharacterCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CharacterCard() {
+  return (
+    <div className="CharacterCard">
+      <h1>Character</h1>
+    </div>
+  );
+}
+
+export default CharacterCard;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SearchBar() {
+  return (
+    <div className="SearchBar">
+      <h1>Search</h1>
+    </div>
+  );
+}
+
+export default SearchBar;
