c14b42103c1dd8e0d6185e113668595b7e801c47
Elvis Lumnica <58716184+elumnica@users.noreply.github.com>
2020-09-02 18:02:13 -0400

    added media queries to create mobile responsive design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBar from './components/SearchBar';
 import CharacterCard from './components/CharacterCard';
 import { Container, Row } from 'react-bootstrap';
 import SelectionModal from './components/SelectionModal';
+import ResponsiveEmbed from 'react-bootstrap/ResponsiveEmbed';
 import './app.css';
 
 const proxy = process.env.REACT_APP_API_URL;
-const key = process.env.REACT_APP_API_KEY;
+const key = 100964191747940;
 
 function App() {
   const [characterInfo, setCharacterInfo] = useState(null);
@@ -14,14 +15,14 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [show, setShow] = useState(false);
   const [characterList, setCharacterList] = useState(null);
-
+ 
   console.log('characterInfo', characterInfo);
 
   const getCharacter = async (characterName) => {
     setIsLoading(true);
     try {
       const response = await fetch(
-        `${proxy}https://superheroapi.com/api/${key}/search/${characterName}`
+        `https://superheroapi.com/api/${key}/search/${characterName}`
       );
       const data = await response.json();
       if (!response.ok || data.response === 'error') {
@@ -45,8 +46,8 @@ function App() {
   };
 
   return (
-    <div className="parent">
-      <Container fluid className="col-xs-12 col-sm-4">
+    <div className='parent'>
+      <Container fluid>
         <div className='card'>
           <Row>
             <h1 className='title'>Heroes and Villains</h1>
@@ -71,3 +72,5 @@ function App() {
 }
 
 export default App;
+
+
